f5a2eed86ef14180802fdb6d3aa68dc5fd18ecdb
shrikant1905 <sullereshrikant19@gmail.com>
2026-05-07 18:55:22 +0530

    fix: FORCE hardcoded db connection

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || process.env.MYSQLHOST || 'crossover.proxy.rlwy.net',
-    user: process.env.DB_USER || process.env.MYSQLUSER || 'root',
-    password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD || 'MvOVbhrwgsGEHJsgNClqWkjdsJRHdVtY',
-    database: process.env.DB_NAME || process.env.MYSQLDATABASE || 'railway',
-    port: parseInt(process.env.DB_PORT || process.env.MYSQLPORT || '14238'),
+    host: 'crossover.proxy.rlwy.net',
+    user: 'root',
+    password: 'MvOVbhrwgsGEHJsgNClqWkjdsJRHdVtY',
+    database: 'railway',
+    port: 14238,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
